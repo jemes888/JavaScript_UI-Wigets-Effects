06aab7b07c020ff8e9f8b06861df5a96a76ffe49
Jemes Mondol <jemes888@gmail.com>
2019-03-13 13:00:42 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,141 @@
+  $( function() {
+    var icons = {
+      header: "ui-icon-circle-arrow-e",
+      activeHeader: "ui-icon-circle-arrow-s"
+    };
+    $( "#accordion" ).accordion({
+      icons: icons
+    });
+    $( "#toggle" ).button().on( "click", function() {
+      if ( $( "#accordion" ).accordion( "option", "icons" ) ) {
+        $( "#accordion" ).accordion( "option", "icons", null );
+      } else {
+        $( "#accordion" ).accordion( "option", "icons", icons );
+      }
+    });
+  } );
+  // =========================================
+    $( function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    function split( val ) {
+      return val.split( /,\s*/ );
+    }
+    function extractLast( term ) {
+      return split( term ).pop();
+    }
+ 
+    $( "#tags" )
+      // don't navigate away from the field on tab when selecting an item
+      .on( "keydown", function( event ) {
+        if ( event.keyCode === $.ui.keyCode.TAB &&
+            $( this ).autocomplete( "instance" ).menu.active ) {
+          event.preventDefault();
+        }
+      })
+      .autocomplete({
+        minLength: 0,
+        source: function( request, response ) {
+          // delegate back to autocomplete, but extract the last term
+          response( $.ui.autocomplete.filter(
+            availableTags, extractLast( request.term ) ) );
+        },
+        focus: function() {
+          // prevent value inserted on focus
+          return false;
+        },
+        select: function( event, ui ) {
+          var terms = split( this.value );
+          // remove the current input
+          terms.pop();
+          // add the selected item
+          terms.push( ui.item.value );
+          // add placeholder to get the comma-and-space at the end
+          terms.push( "" );
+          this.value = terms.join( ", " );
+          return false;
+        }
+      });
+  } );
+    // ======================================
+  $( function() {
+    $( ".widget button" )
+      .eq( 0 ).button()
+      .end().eq( 1 ).button( {
+        icon: "ui-icon-gear",
+        showLabel: false
+      } ).end().eq( 2 ).button( {
+        icon: "ui-icon-gear"
+      } ).end().eq( 3 ).button( {
+        icon: "ui-icon-gear",
+        iconPosition: "end"
+      } ).end().eq( 4 ).button( {
+        icon: "ui-icon-gear",
+        iconPosition: "top"
+      } ).end().eq( 5 ).button( {
+        icon: "ui-icon-gear",
+        iconPosition: "bottom"
+      } );
+  } );
+    // ======================================
+      $( function() {
+    $( "input" ).checkboxradio();
+    $( "fieldset" ).controlgroup();
+  } );
+
+
+    // ======================================
+  $( function() {
+    $( ".controlgroup" ).controlgroup()
+    $( ".controlgroup-vertical" ).controlgroup({
+      "direction": "vertical"
+    });
+  } );
+    // ======================================
+  $( function() {
+    $( "#datepicker" ).datepicker({
+      showButtonPanel: true
+    });
+  } );
+    // ======================================
+      $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } );
+    // ======================================
+
